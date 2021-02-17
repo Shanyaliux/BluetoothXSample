43ce74c95a730cd9533578b1999a4a5f11cf5bde
Shanyaliux <445502929@qq.com>
2021-02-17 13:01:55 +0800

    test jcenter

--- BluetoothX/src/main/java/world/shanya/bluetoothx/BluetoothX.java
+++ BluetoothX/src/main/java/world/shanya/bluetoothx/BluetoothX.java
@@ -1,6 +1,11 @@
 package world.shanya.bluetoothx;
 
 public class BluetoothX {
+
+    public BluetoothX() {
+        hello();
+    }
+
     void hello(){
         System.out.println("HelloWorld");
     }
